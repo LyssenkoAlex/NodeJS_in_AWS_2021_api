52d9e375222718112eb392d28bce35616a31c54b
Alexandr_Lyssenko <ALexandr>
2021-08-25 18:01:04 +0600

    jest fix

--- product-service/__tests__/handler.test.js
+++ product-service/__tests__/handler.test.js
@@ -3,8 +3,16 @@ const productList = require('../src/productList.json');
 
 
 test('check getProductsList response', async () => {
+    let expectedJson = {
+        count: 2,
+        description: "Product 2 description",
+        id: "f6d593a0-4e46-4a77-9062-057a8645ace1",
+        logo: "https://r2.readrate.com/img/pictures/basic/792/792600/7926008/w800h317-89405d1d.jpg",
+        price: 2000,
+        title: "Product 2"}
+
     let functionResponse = await handlerDQL.getAllProducts();
-    expect(JSON.parse(functionResponse.body)).toStrictEqual(productList);
+    expect(JSON.parse(functionResponse.body)[0]).toStrictEqual(expectedJson);
 });
 
 test('check getProductById response', async () => {
