97f1d94530e53a14e73c25336f3b5842e8ddf8dc
Alexandr_Lyssenko <ALexandr>
2021-09-04 16:19:38 +0600

    fic joi check

--- product-service/src/utils/productSchema.js
+++ product-service/src/utils/productSchema.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 export const productDataSchema = Joi.object({
     title: Joi.string().required(),
     description: Joi.string().required(),
-    price: Joi.number().positive().greater(1).precision(2).required(),
-    count: Joi.number().positive().greater(1).precision(2).required(),
+    price: Joi.number().positive().greater(0).precision(2).required(),
+    count: Joi.number().positive().greater(0).precision(2).required(),
 });
 
